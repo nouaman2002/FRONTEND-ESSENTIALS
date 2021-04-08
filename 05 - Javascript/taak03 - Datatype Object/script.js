f5f0ea55033cdfa8c42225e0085dc1a9378b493d
Nouaman <nouaman.abdelghani@student.rocva.nl>
2021-04-08 13:05:26 +0200

    datatype afgerond

---
+++ 05 - Javascript/taak03 - Datatype Object/script.js
@@ -0,0 +1,17 @@
+
+
+let student = {
+    voornaam: "nouaman",
+    achternaam: "abdelghani",
+    leeftijd: 18,
+    ikBenStemGerechtig: true,
+    ikMagLegaalAlcoholKopen: true,
+    ikMagBrommerRijden: true,
+
+
+}
+
+
+console.log(student);
+
+console.log(student.leeftijd);
---
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -0,0 +1,8 @@
+let: mijnvoornaam = "nouaman"; 
+let: mijnachternaam = "abdelghani";
+let: mijnleeftijd = 18;
+
+
+
+
+console.log("Mijn naam is `<voornaam>` `<achternaam>` en mijn leeftijd is `<leeftijd>` jaar");
