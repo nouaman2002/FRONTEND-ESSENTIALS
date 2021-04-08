22550065721846ffa787848d3b8eb7a104587e21
Nouaman <nouaman.abdelghani@student.rocva.nl>
2021-04-08 12:21:21 +0200

    taak 2 gedaan

---
+++ 05 - Javascript/taak01 - Console.log/script.js
@@ -0,0 +1,2 @@
+console.log("Mijn naam is nouaman.__ Dit is mijn eerste consolebericht"
+);
---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,12 @@
+let voornaam = "nouaman";
+let achternaam = "abdelghani";
+let leeftijd = 18;
+let ikBenStemGerechtig = true;
+let ikMagLegaalAlcoholKopen = true;
+let ikMagBrommerRijden = true;
+let  SoftwareOntwikkeling = null;
+
+
+leeftijd = leeftijd - 12;
+
+console.log(leeftijd);
